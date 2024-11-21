9adb680bd1b140c9badc77d4a97938ba979a2c54
rp222nc <rp222nc@student.lnu.se>
2024-11-21 12:14:03 +0100

    GREEN Check task is string

--- __tests__/TodoItemTest.js
+++ __tests__/TodoItemTest.js
@@ -2,9 +2,8 @@ import TodoItem from '../src/TodoItem'
 
 describe('TodoItem class test', () => {
   it('should set a new todo task', () => {
-    const todoItem = new TodoItem()
     const task = 'Buy milk'
-    todoItem.setTask(task)
+    const todoItem = new TodoItem(task)
     const actual = todoItem.getTask()
     expect(actual).toBe(task)
   })
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,15 +2,23 @@ class TodoItem {
     #task
 
     constructor(task) {
+        this.checkTaskName(task)
         this.#task = task
     }
 
     setTask(task) {
+        this.checkTaskName(task)
         this.#task = task
     }
 
     getTask() {
         return this.#task
+    }
+
+    checkTaskName(task) {
+        if (typeof task !== "string") {
+            throw new Error("Task name must be a string")
+        }
     }
 }
 
