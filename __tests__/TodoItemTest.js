918011bea8b8b7a75008dcee8f5612629dcde086
rp222nc <rp222nc@student.lnu.se>
2024-11-21 12:20:29 +0100

    REFACTOR Seperate tests for creating a new task and changing an existing task

--- __tests__/TodoItemTest.js
+++ __tests__/TodoItemTest.js
@@ -1,11 +1,19 @@
 import TodoItem from '../src/TodoItem'
 
 describe('TodoItem class test', () => {
-  it('should set a new todo task', () => {
+  it('should create a new todo task', () => {
     const task = 'Buy milk'
     const todoItem = new TodoItem(task)
     const actual = todoItem.getTask()
     expect(actual).toBe(task)
+  })
+
+  it('should change a task', () => {
+    const todoItem = new TodoItem('Buy milk')
+    const newTask = 'Buy milk'
+    todoItem.setTask(newTask)
+    const actual = todoItem.getTask()
+    expect(actual).toBe(newTask)
   })
 
   it('should throw an exception if task is not a string', () => {
