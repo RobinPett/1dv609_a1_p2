93047c29f301efd44592a20db3e6b1464010fb8f
rp222nc <rp222nc@student.lnu.se>
2024-11-21 14:44:33 +0100

    REFACTOR Create todoItem before each test

--- __tests__/TodoItemTest.js
+++ __tests__/TodoItemTest.js
@@ -1,18 +1,23 @@
 import TodoItem from '../src/TodoItem'
 
+let todoItem
+
+beforeEach(() => {
+  todoItem = new TodoItem('Buy milk')
+})
+
 describe('TodoItem class test', () => {
   it('should create a new todo task', () => {
     const task = 'Buy milk'
-    const todoItem = new TodoItem(task)
     const actual = todoItem.getTask()
     expect(actual).toBe(task)
   })
 
   it('should change a task', () => {
-    const todoItem = new TodoItem('Buy milk')
     const newTask = 'Buy milk'
     todoItem.setTask(newTask)
     const actual = todoItem.getTask()
+
     expect(actual).toBe(newTask)
   })
 
@@ -22,10 +27,19 @@ describe('TodoItem class test', () => {
   })
 
   it('should set task status to done', () => {
-    const todoItem = new TodoItem('Buy milk')
     todoItem.toggleStatus()
     const actual = todoItem.getStatus()
     const expected = 1
+
+    expect(actual).toBe(expected)
+  })
+
+  it ('should set task status to not done', () => {
+    todoItem.toggleStatus() // Status: Done (1)
+    todoItem.toggleStatus() // Status: Not done (0)
+    const actual = todoItem.getStatus()
+    const expected = 0
+
     expect(actual).toBe(expected)
   })
 })
