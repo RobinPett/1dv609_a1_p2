01da2c31feb6b0527e5ef4e79b202c497335757e
rp222nc <rp222nc@student.lnu.se>
2024-11-23 18:50:38 +0100

    RED Add test for removing task in manager

--- __tests__/TodoManagerTest.js
+++ __tests__/TodoManagerTest.js
@@ -44,6 +44,18 @@ describe('TodoManager class test', () => {
         expect(actualTasks[0].getTask()).toBe(task1)
         expect(actualTasks[1].getTask()).toBe(task2)
     })
+
+    it('should remove one todo item from manager', () => {
+        const task1 = 'Buy milk'
+        const task2 = 'Buy bread'
+
+        const todoManager = createAndAddTasksToManager([task1, task2])
+        todoManager.removeTask(task1)
+        const actualTasks = todoManager.getTasks()
+
+        expect(actualTasks.length).toBe(1)
+        expect(actualTasks[0].getTask()).toBe(task2)
+    })
 })
 
 
