6b9d5ff56c0a9eba1f8afce4ab30c1ce8fd1acbd
rp222nc <rp222nc@student.lnu.se>
2024-12-01 21:23:09 +0100

    REFACTOR Move SUT creation outside of test

--- __tests__/TaskStorageTest.js
+++ __tests__/TaskStorageTest.js
@@ -1,9 +1,14 @@
 import TaskStorage from "../src/js/TaskStorage"
 
+let sut
+
+beforeEach(() => {
+    sut = new TaskStorage()
+})
+
 describe('TaskStorage Test', () => {
     it ('should save one task', () => {
         const task = {name: 'Buy Milk', id: 'mockId', completed: false}
-        const sut = new TaskStorage()
         sut.save([task])
 
         const savedTasks = sut.load()
