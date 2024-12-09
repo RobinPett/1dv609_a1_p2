3fc2eca8d2c4fcc1904e0320a5d175d61a35bc52
rp222nc <rp222nc@student.lnu.se>
2024-12-09 13:06:19 +0100

    RED Test for rendering a single task

---
+++ __tests__/TaskUITest.js
@@ -0,0 +1,35 @@
+import TaskUI from '../src/TaskUi'
+import Task from '../src/js/Task'
+
+const mockTaskManager = {
+    getTasks: jest.fn()
+}
+
+jest.mock('../src/js/Task', () => {
+    return {
+        __esModule: true,
+        default: class MockTask {
+            constructor(name) {
+                this.name = name
+                this.id = 'mockId'
+                this.completed = false
+            }
+            getName() { return this.name }
+            getId() { return this.id }   
+            isCompleted() { return this.completed }
+        }
+    }
+})
+
+describe('TaskUI Test', () => {
+    it ('should render one task', () => {
+        const mockTask = new Task('Buy milk')
+
+        mockTaskManager.getTasks.mockReturnValue(mockTask)
+        const ui = new TaskUI(document, mockTaskManager)
+
+        ui.renderTasks()
+
+        expect(document.innerHTML).toContain('Buy milk')
+    })
+})
