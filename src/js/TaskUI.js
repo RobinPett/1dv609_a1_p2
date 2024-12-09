1394e21f2b6149525a54d11a02d896c47ea1b4bf
rp222nc <rp222nc@student.lnu.se>
2024-12-09 21:15:22 +0100

    REFACTOR Add private fields for manager and document

--- src/js/TaskUI.js
+++ src/js/TaskUI.js
@@ -1,28 +1,31 @@
 class TaskUI {
+    #document
+    #taskManager
+
     constructor(document, taskManager) {
-        this.document = document
-        this.taskManager = taskManager
+        this.#document = document
+        this.#taskManager = taskManager
     }
 
     renderTasks() {
-        const taskList = this.document.createElement('div')
+        const taskList = this.#document.createElement('div')
         taskList.setAttribute('id', 'task-list')
 
-        const allTasks = this.taskManager.getTasks()
+        const allTasks = this.#taskManager.getTasks()
 
         allTasks.forEach(task => {
-            const listElement = this.document.createElement('li')
+            const listElement = this.#document.createElement('li')
             listElement.innerHTML = task.getName()
             listElement.setAttribute('id', task.getId())
 
-            const checkbox = this.document.createElement('input')
+            const checkbox = this.#document.createElement('input')
             checkbox.setAttribute('type', 'checkbox')
             listElement.appendChild(checkbox)
 
             taskList.appendChild(listElement)
         })
 
-        this.document.body.appendChild(taskList)
+        this.#document.body.appendChild(taskList)
     }
 }
 
