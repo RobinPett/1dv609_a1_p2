8eb11bd6097bc3eabf1bc20ede3e12b8ca991776
rp222nc <rp222nc@student.lnu.se>
2024-12-13 14:44:14 +0100

    RED Test for ui to be renderd

--- __tests__/indexTest.js
+++ __tests__/indexTest.js
@@ -1,8 +1,21 @@
 import start from "../src/js"
+import TaskUI from "../src/js/TaskUI"
+
+jest.mock('../src/js/TaskUI', () => {
+    return {
+        renderUI: jest.fn()
+    }
+})
 
 describe('Task class test', () => {
     it('should have a start function', () => {
         expect(start).toBeDefined()
         expect(typeof(start)).toBe('function')
     })
+
+    it('should render ui when running start function', () => {
+        start()
+        expect(TaskUI.renderUI).toHaveBeenCalled()
+    })
+
 })
