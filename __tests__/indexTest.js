292c47f258fb757b6abe6bac8eeecd7920192262
rp222nc <rp222nc@student.lnu.se>
2024-12-13 14:23:58 +0100

    GREEN Create a start function

--- __tests__/indexTest.js
+++ __tests__/indexTest.js
@@ -1,6 +1,7 @@
+import start from "../src/js"
 
 describe('Task class test', () => {
     it('should have a start function', () => {
-        expect(start()).toBeDefined()
+        expect(start).toBeDefined()
     })
 })
