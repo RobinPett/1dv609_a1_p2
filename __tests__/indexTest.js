3218752f35a67e639966cf2c3a4b359b6bbf5a8d
rp222nc <rp222nc@student.lnu.se>
2024-12-14 14:30:28 +0100

    RED Test for executing start when index.js is loaded

--- __tests__/indexTest.js
+++ __tests__/indexTest.js
@@ -1,5 +1,3 @@
-import start from "../src/js"
-
 const mockTaskUIInstance = {
     renderUI: jest.fn()
 }
@@ -8,7 +6,19 @@ jest.mock('../src/js/TaskUI', () => {
     return jest.fn().mockImplementation(() => mockTaskUIInstance)
 })
 
-describe('Task class test', () => {
+let start
+
+beforeEach(() => {
+    jest.clearAllMocks()
+    jest.resetModules()
+})
+
+describe('Index.js test', () => {
+    beforeEach(async () => {
+        const module = await import('../src/js/index.js')
+        start = module.default
+    })
+
     it('should have a start function', () => {
         expect(start).toBeDefined()
         expect(typeof(start)).toBe('function')
@@ -16,6 +26,10 @@ describe('Task class test', () => {
 
     it('should render ui when running start function', () => {
         start()
+        expect(mockTaskUIInstance.renderUI).toHaveBeenCalled()
+    })
+
+    it('should render ui directly when index.js is loaded', async () => {
         expect(mockTaskUIInstance.renderUI).toHaveBeenCalled()
     })
 })
